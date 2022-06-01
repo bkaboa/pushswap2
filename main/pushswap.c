90c83ec006bb70f5e6ef9d526f6ab60cff3bbe74
Charles Zang <czang@z1r11p1.42lyon.fr>
2022-06-01 22:43:30 +0200

    t

--- main/pushswap.c
+++ main/pushswap.c
@@ -2,7 +2,8 @@
 
 int	main(int argc, char **argv)
 {
-	t_all	all;
+	t_all		all;
+	t_add_stack	add;
 
 	errno = 1;
 	if (argc <= 2)
@@ -15,13 +16,13 @@ int	main(int argc, char **argv)
 		return (perso_errno_msg());
 	all.f_index = first_sort(all.num2, all.total_index);
 	init_struct(&all);
-	if (all.total_index >= 500)
-		sort_upper500(&all);
-	else if (all->total_index >= 100)
-		sort_upper100(&all);
-	else if (all->total_index >= 4)
-		sort_under_100(&all);
-	else
+	//if (all.total_index >= 500)
+	//	sort_upper500(&all);
+	//else if (all->total_index >= 100)
+	//	sort_upper100(&all);
+	//else if (all->total_index >= 4)
+	//	sort_under_100(&all);
+	//else
 		sort_3_num(&all);
 	free_stk(all.stk_a);
 	return (0);
